0c02af4dface0813e2a95546e4677d43a9fb6fe0
Hidoi-Chan <HidoiChan@mail.ru>
2020-10-10 19:59:01 +0300

    Toggle/like/rate buttons

--- src/form-elements.js
+++ src/form-elements.js
@@ -7,7 +7,10 @@ import '@blocks/button/button.scss'
 import '@blocks/dropdown/dropdown.scss'
 import dropdown from '@blocks/dropdown/dropdown.js'
 import '@blocks/checkbox/checkbox.scss'
-import '@blocks/radio-buttons/radio-buttons.scss'
+import '@blocks/radio/radio.scss'
+import '@blocks/toggle/toggle.scss'
+import '@blocks/like-button/like-button.scss'
+import '@blocks/rate/rate.scss'
 
 dropdown('guests')
 dropdown('rooms')
