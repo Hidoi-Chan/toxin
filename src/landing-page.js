f82869ceb6d91882eafec8a73de5cd0780d070a2
Hidoi-Chan <HidoiChan@mail.ru>
2020-12-04 19:43:52 +0300

    Responsive header: burger menu

--- src/landing-page.js
+++ src/landing-page.js
@@ -7,11 +7,11 @@ import '@blocks/button/button.scss'
 import '@blocks/sign-in/sign-in.scss'
 import '@blocks/nav/nav.scss'
 import '@blocks/nav/nav.js'
+import '@blocks/burger/burger.scss'
+import '@blocks/burger/burger.js'
 import '@blocks/header/header.scss'
 import '@blocks/card/card.scss'
 import '@blocks/field/field.scss'
 import '@blocks/copyright-bar/copyright-bar.scss'
 import '@blocks/footer/footer.scss'
 import '@pages/landing-page/landing-page.scss'
-
-console.log(1)
